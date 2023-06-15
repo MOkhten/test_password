26b20baf291718e872f873f293ea11d43f64252f
MOkhten <m.okhten@gmail.com>
2023-06-16 00:05:06 +0300

    some replacements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+// import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatCardModule } from '@angular/material/card';
 
 
 import { AppComponent } from './app.component';
@@ -17,10 +17,6 @@ import {FormComponent} from './components/form.component'
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
    
   ],
   providers: [],
